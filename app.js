c6970a86742fbc4df6fe8939a7d43c0a2d789793
Rodion Sibov <rodionsibov@gmail.com>
2021-12-06 11:40:04 +0200

    update

---
+++ app.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const gridDisplay = document.querySelector('.grid')
+    const scoreDisplay = document.querySelector('#score')
+    const resultDisplay = document.querySelector('#result')
+    const width = 4
+    let squares = []
+
+    // create a playing board
+    function createBord() {
+        for(let i = 0; i < width * width; i++) {
+            const square = document.createElement('div')
+            square.innerHTML = 0
+            gridDisplay.appendChild(square)
+            squares.push(square)
+        }
+    }
+})
