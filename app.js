3dda805d72d0652c3ebec0dd4a662c965eb62647
Rodion Sibov <rodionsibov@gmail.com>
2021-12-06 15:02:55 +0200

    update

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
             squares.push(square)
         }
         generate()
+        generate()
     }
     createBord()
 
@@ -24,4 +25,27 @@ document.addEventListener('DOMContentLoaded', () => {
             squares[randomNumber].innerHTML = 2
         } else generate()
     }
+
+    // swipe right
+    function moveRight() {
+        for (let i = 0; i < 16; i++) {
+            if (i % 4 === 0) {
+                let totalOne = squares[i].innerHTML
+                let totalTwo = squares[i + 1].innerHTML
+                let totalThree = squares[i + 2].innerHTML
+                let totalFour = squares[i + 3].innerHTML
+                let row = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
+
+                console.log(row);
+            }
+        }
+    }
+
+    moveRight()
+
+
+
+
+
+
 })
